d0e43a9636eedd5c9a7543e8b827f0f2ec9a3431
Priyadharshini Vitoba <priyavitoba@gmail.com>
2025-08-13 04:13:46 +0000

    add_functions

--- srv/service.cds
+++ srv/service.cds
@@ -1,4 +1,8 @@
-
+@protocol: `rest`
 service product {
 function printhelloworld( input: String) returns String;
+function add( num1: Integer, num2:Integer) returns Integer;
+@open
+type obj{};
+function myfunction(category:Integer) returns obj;
 }
--- srv/service.js
+++ srv/service.js
@@ -1,11 +1,36 @@
 const cds = require('@sap/cds');
 
-module.exports = cds.service.impl( async function(srv) {
-    srv.on('printhelloworld', req => { 
+module.exports = cds.service.impl(async function (srv) {
+
+    srv.on('printhelloworld', req => {
         console.log(req.data.input)
         return `${req.data.input} world`
     })
-    
+
+    srv.on('add', req => {
+        console.log(req.data)
+        let result = req.data.num1 + req.data.num2;
+        return result
+    })
+    srv.on('myfunction', req => {
+        console.log(req.data.category)
+        let res = {}
+        if (req.data.category == 1) {
+            res.product = 'BMW',
+                res.price = '120',
+                res.loc = 'Dallas'
+        }        else
+         {
+            res.product = 'Audi',
+            res.stock = '100',
+            res.pricearray = [{
+                "price": "100",
+                "discount": "20%"
+            }]
+        }
+        console.log( res);
+        return res;
+    })
 })
 
 
